cffd882b74df44b8febc9f27ebc4bf449c69aede
Beez131Github <olivercollins123@hotmail.com>
2024-11-01 12:12:36 +1300

    custom tinker

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import customtkinter as ctk
 import tkinter as tk
 from functions import offsetDims, screenDims, writeFile, readFile, sendMessage
 import pyautogui
@@ -10,263 +11,208 @@ main_dir = os.path.dirname(os.path.abspath(__file__))
 class GUI:
     def __init__(self, font="Courier"):
         self.font = font
-
         self.connected = False
 
     def initWindow(self):
         sendMessage("Started alt!")
         
-        self.window = tk.Tk()
-
+        self.window = ctk.CTk()
         self.window.title("Beez Universal Macro - Alt")
-
-        # Set the GUI logo
+        
         logo_path = os.path.join(main_dir, "basicbeeface.ico")
         self.window.iconbitmap(logo_path)
         
         self.window.geometry(f"{offsetDims(700, 'x')}x{offsetDims(350, 'y')}")
+        self.window.resizable(False, False)
 
-        self.window.resizable(False, False) #Locks GUI size
-
-        ###### CREATING TABS ######
-
-        self.tabControl = tk.ttk.Notebook(self.window)
-
-        self.joinSettingsTab = tk.ttk.Frame(self.tabControl)
-        self.connectTab = tk.ttk.Frame(self.tabControl)
-        self.settingsTab = tk.ttk.Frame(self.tabControl)
-        self.privateServersTab = tk.ttk.Frame(self.tabControl)
-        self.creditsTab = tk.ttk.Frame(self.tabControl)
-
-        self.tabControl.add(self.joinSettingsTab, text='Join Settings')
-        self.tabControl.add(self.connectTab, text='Connecting')
-        self.tabControl.add(self.settingsTab, text='Settings')
-        self.tabControl.add(self.privateServersTab, text='Private Servers')
-        self.tabControl.add(self.creditsTab, text='Credits')
-
-        ###### CREATING TEXT ######
+        self.tabControl = ctk.CTkTabview(self.window)
+        self.tabControl.add(name='Join Settings')
+        self.tabControl.add(name='Connecting')
+        self.tabControl.add(name='Settings')
+        self.tabControl.add(name='Private Servers')
+        self.tabControl.add(name='Credits')
 
         for i in range(1, 6):
             globals()[f"self.{i}"] = 0
 
         self.maxLoadTime = tk.StringVar()
 
-        self.joinTitle = tk.Label(self.joinSettingsTab, text="Join Settings")
-        self.joinTitle.config(font=(self.font, 17))
+        self.joinTitle = ctk.CTkLabel(self.tabControl.tab('Join Settings'), text="Join Settings")
+        self.joinTitle.configure(font=(self.font, 24))
 
-        self.maxLoadText = tk.Label(self.joinSettingsTab, text="Maximum load time:")
-        self.maxLoadText.config(font=(self.font, 14))
+        self.maxLoadText = ctk.CTkLabel(self.tabControl.tab('Join Settings'), text="Maximum load time:")
+        self.maxLoadText.configure(font=(self.font, 14))
 
-        self.maxLoad = tk.Entry(self.joinSettingsTab, width=3)
+        self.maxLoad = ctk.CTkEntry(self.tabControl.tab('Join Settings'), width=40)
 
         try:
             self.maxLoad.insert(0, readFile("guiFiles/maxLoadTime.txt"))
         except:
             self.maxLoad.insert(0, "10")
 
-        self.start = tk.Button(self.window, text="Start (f1)", command=self.startMacro)
-        self.stop = tk.Button(self.window, text="Stop (f2)", command=self.stopMacro)
+        self.start = ctk.CTkButton(self.window, text="Start (f1)", command=self.startMacro)
+        self.stop = ctk.CTkButton(self.window, text="Stop (f2)", command=self.stopMacro)
 
-        self.connectTitle = tk.Label(self.connectTab, text="Connecting")
-        self.connectTitle.config(font=(self.font, 17))
+        self.connectTitle = ctk.CTkLabel(self.tabControl.tab('Connecting'), text="Connecting")
+        self.connectTitle.configure(font=(self.font, 24))
 
-        self.ipText = tk.Label(self.connectTab, text="Host name (found in main, connecting tab):")
-        self.ipText.config(font=(self.font, 14))
+        self.ipText = ctk.CTkLabel(self.tabControl.tab('Connecting'), text="Host name (found in main, connecting tab):")
+        self.ipText.configure(font=(self.font, 14))
 
-        self.ip = tk.Entry(self.connectTab, width=10)
+        self.ip = ctk.CTkEntry(self.tabControl.tab('Connecting'), width=120)
 
         try:
             self.ip.insert(0, readFile("guiFiles/hostName.txt"))
         except:
             pass
 
-        self.portText = tk.Label(self.connectTab, text="Port:")
-        self.portText.config(font=(self.font, 14))
+        self.portText = ctk.CTkLabel(self.tabControl.tab('Connecting'), text="Port:")
+        self.portText.configure(font=(self.font, 14))
 
-        self.port = tk.Entry(self.connectTab, width=10)
+        self.port = ctk.CTkEntry(self.tabControl.tab('Connecting'), width=120)
 
         try:
             self.port.insert(0, readFile("guiFiles/port.txt"))
         except:
             self.port.insert(0, "")
 
-        self.connect = tk.Button(self.connectTab, text="Connect to main", command=self.connectToMain)
+        self.connect = ctk.CTkButton(self.tabControl.tab('Connecting'), text="Connect to main", command=self.connectToMain)
 
-        self.settingsTitle = tk.Label(self.settingsTab, text="Settings")
-        self.settingsTitle.config(font=(self.font, 17))
+        self.settingsTitle = ctk.CTkLabel(self.tabControl.tab('Settings'), text="Settings")
+        self.settingsTitle.configure(font=(self.font, 24))
 
-        self.webhookText = tk.Label(self.settingsTab, text="Discord webhook:")
-        self.webhookText.config(font=(self.font, 14))
+        self.webhookText = ctk.CTkLabel(self.tabControl.tab('Settings'), text="Discord webhook:")
+        self.webhookText.configure(font=(self.font, 14))
 
-        self.webhook = tk.Entry(self.settingsTab, width=10)
+        self.webhook = ctk.CTkEntry(self.tabControl.tab('Settings'), width=200)
 
         try:
             self.webhook.insert(0, readFile("guiFiles/webhook.txt"))
         except:
             self.webhook.insert(0, "")
 
-        self.mode = tk.StringVar(self.settingsTab)
+        self.mode = tk.StringVar(self.tabControl.tab('Settings'))
         self.mode.set("night searcher")
 
-        self.modeText = tk.Label(self.settingsTab, text="Mode:")
-        self.modeText.config(font=(self.font, 14))
+        self.modeText = ctk.CTkLabel(self.tabControl.tab('Settings'), text="Mode:")
+        self.modeText.configure(font=(self.font, 14))
 
-        self.modeBox = tk.OptionMenu(self.settingsTab, self.mode, *["night searcher", "night waiter"])
+        self.modeBox = ctk.CTkOptionMenu(self.tabControl.tab('Settings'), 
+                                       values=["night searcher", "night waiter"],
+                                       variable=self.mode)
 
-        self.privateServersText = tk.Label(self.privateServersTab, text="Private servers")
-        self.privateServersText.config(font=(self.font, 14))
+        self.privateServersText = ctk.CTkLabel(self.tabControl.tab('Private Servers'), text="Private servers")
+        self.privateServersText.configure(font=(self.font, 24))
 
         for i in range(1, 6):
-            globals()[f"self.{i}"] = tk.Entry(self.privateServersTab)
-
+            globals()[f"self.{i}"] = ctk.CTkEntry(self.tabControl.tab('Private Servers'), width=200)
             try:
                 globals()[f"self.{i}"].insert(0, self.getPrivateServer(i - 1))
-
             except:
                 pass
 
-        self.usingPs = tk.IntVar(self.privateServersTab)
-
-        self.usingPsBox = tk.Checkbutton(self.privateServersTab, text="Use private servers", variable=self.usingPs, onvalue=1, offvalue=0)
+        self.usingPs = tk.IntVar(self.tabControl.tab('Private Servers'))
+        self.usingPsBox = ctk.CTkCheckBox(self.tabControl.tab('Private Servers'), 
+                                         text="Use private servers", 
+                                         variable=self.usingPs)
 
         try:
             self.usingPs.set(1 if eval(readFile("guiFiles/joinPrivateServers.txt")) else 0)
-
         except:
             pass
 
         owner = "Beez131"
+        contributors = ["Sharkboy1663", "Pirosow"]
+        specialThanks = ["Slymi", "Fire_king66", "Lvl18BubbleBee"]
 
-        contributors = [
-            "Sharkboy1663",
-            "Pirosow"
-        ]
+        self.ownerText = ctk.CTkLabel(self.tabControl.tab('Credits'), text="Owner/Head Developer:")
+        self.ownerText.configure(font=(self.font, 15))
 
-        specialThanks = [
-            "Slymi",
-            "Fire_king66",
-            "Lvl18BubbleBee",
-        ]
+        self.owner = ctk.CTkLabel(self.tabControl.tab('Credits'), text=owner)
 
-        self.ownerText = tk.Label(self.creditsTab, text="Owner/Head Developer:")
-        self.ownerText.config(font=(self.font, 15))
-
-        self.owner = tk.Label(self.creditsTab, text=owner)
-
-        self.contributors = tk.Label(self.creditsTab, text="Developers:")
-        self.contributors.config(font=(self.font, 15))
+        self.contributors = ctk.CTkLabel(self.tabControl.tab('Credits'), text="Developers:")
+        self.contributors.configure(font=(self.font, 15))
 
         for contributor in contributors:
-            globals()[f"self.{contributor}"] = tk.Label(self.creditsTab, text=contributor)
+            globals()[f"self.{contributor}"] = ctk.CTkLabel(self.tabControl.tab('Credits'), text=contributor)
 
-        self.specialThanks = tk.Label(self.creditsTab, text="Special Thanks To:")
-        self.specialThanks.config(font=(self.font, 15))
+        self.specialThanks = ctk.CTkLabel(self.tabControl.tab('Credits'), text="Special Thanks To:")
+        self.specialThanks.configure(font=(self.font, 15))
 
         for specialThank in specialThanks:
-            globals()[f"self.{specialThank}"] = tk.Label(self.creditsTab, text=specialThank)
+            globals()[f"self.{specialThank}"] = ctk.CTkLabel(self.tabControl.tab('Credits'), text=specialThank)
 
-        ###### DISPLAYING TEXT ######
-
+        # Pack everything
         self.tabControl.pack(expand=2, fill="both")
-
-        self.joinTitle.pack()
-
-        self.maxLoadText.pack()
-        self.maxLoad.pack()
-
-        self.maxLoadText.place(relx=0.5, rely=0.35, anchor="n")
-        self.maxLoad.place(relx=0.5, rely=0.5, anchor="n")
-
-        self.start.pack()
-        self.start.place(relx=0.35, rely=0.8, anchor="n")
-
-        self.stop.pack()
-        self.stop.place(relx=0.65, rely=0.8, anchor="n")
-
-        self.connectTitle.pack()
-
-        self.ipText.pack()
-        self.ip.pack()
-
-        self.portText.pack()
-        self.port.pack()
-
-        self.connect.pack()
-
-        self.settingsTitle.pack()
-
-        self.webhookText.pack()
-        self.webhook.pack()
-
-        self.modeText.pack()
-        self.modeBox.pack()
-
-        self.modeText.place(relx="0.5", rely="0.45", anchor="n")
-        self.modeBox.place(relx="0.5", rely="0.6", anchor="n")
-
-        self.privateServersText.pack()
-
+        
+        self.joinTitle.pack(pady=10)
+        self.maxLoadText.pack(pady=5)
+        self.maxLoad.pack(pady=5)
+        
+        self.start.pack(side="left", padx=20, pady=10)
+        self.stop.pack(side="right", padx=20, pady=10)
+        
+        self.connectTitle.pack(pady=10)
+        self.ipText.pack(pady=5)
+        self.ip.pack(pady=5)
+        self.portText.pack(pady=5)
+        self.port.pack(pady=5)
+        self.connect.pack(pady=10)
+        
+        self.settingsTitle.pack(pady=10)
+        self.webhookText.pack(pady=5)
+        self.webhook.pack(pady=5)
+        self.modeText.pack(pady=5)
+        self.modeBox.pack(pady=5)
+        
+        self.privateServersText.pack(pady=10)
         for i in range(5):
-            globals()[f"self.{i + 1}"].pack()
-
-        self.usingPsBox.pack()
-
-        self.ownerText.pack()
-        self.owner.pack()
-
-        self.contributors.pack()
-
+            globals()[f"self.{i + 1}"].pack(pady=5)
+        self.usingPsBox.pack(pady=10)
+        
+        self.ownerText.pack(pady=5)
+        self.owner.pack(pady=2)
+        self.contributors.pack(pady=5)
         for contributor in contributors:
-            globals()[f"self.{contributor}"].pack()
-
-        self.specialThanks.pack()
-
+            globals()[f"self.{contributor}"].pack(pady=2)
+        self.specialThanks.pack(pady=5)
         for specialThank in specialThanks:
-            globals()[f"self.{specialThank}"].pack()
+            globals()[f"self.{specialThank}"].pack(pady=2)
 
+    # Rest of the methods remain the same since they handle logic rather than UI
     def maxLoadTimeChange(self):
         tm = self.maxLoad.get()
-
         if str(tm).isnumeric():
             writeFile("guiFiles/maxLoadTime.txt", tm)
-
         else:
             pyautogui.alert("Make sure to set max load time to a number!")
 
     def ipChange(self):
         ip = self.ip.get()
-
         writeFile("guiFiles/hostName.txt", ip)
 
     def webhookChange(self):
         webhook = self.webhook.get()
-
         writeFile("guiFiles/webhook.txt", webhook)
 
     def portChange(self):
         port = self.port.get()
-
         writeFile("guiFiles/port.txt", port)
 
     def modeChange(self):
         mode = self.mode.get()
-
         writeFile("guiFiles/mode.txt", mode)
 
     def privateServersChange(self):
         privateServers = []
-
         for i in range(5):
             privateServer = globals()[f"self.{i + 1}"].get()
-
             privateServers.append(privateServer)
-
         writeFile("guiFiles/privateServers.txt", str(privateServers))
         writeFile("guiFiles/joinPrivateServers.txt", True if self.usingPs.get() else False)
 
     def getPrivateServer(self, n):
         privateServers = eval(readFile("guiFiles/privateServers.txt"))
-
         return privateServers[n]
 
     def startMacro(self, main=False):
@@ -279,23 +225,17 @@ class GUI:
 
         if self.connected:
             self.Alt.searchForNight()
-
         elif not main:
             pyautogui.alert("Dont forget to connect to main! (connecting tab)")
-
 
     def stopMacro(self):
         quit()
 
     def connectToMain(self):
         self.Alt = altScript.Alt(self.ip.get(), int(self.port.get()), int(self.maxLoad.get()))
-
         time.sleep(0.01)
-
         try:
             self.Alt.connectToMain()
-
             self.connected = True
-
         except:
             pass
